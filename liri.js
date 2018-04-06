3d9db1d70125254e9198ab5dae6680b576138589
Eva Hernandez <Eva@Dylans-MacBook-Pro.local>
2018-04-05 17:29:14 -0700

    Finished with the variables I think

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 var keys = require("./keys.js");
 var request = require("request");
 var Spotify = require("node-spotify-api");
@@ -6,3 +7,6 @@ var Twitter = require("twitter");
 var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+var inputString = process.argv;
+var operand = inputString[2];
